ca427c08a517cb9e3a698850ab039396461d14f3
lee <aznedac2@gmail.com>
2020-07-17 21:40:57 +0900

    second impact

--- routes/lee.js
+++ routes/lee.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('hey man');
+  res.send('hey buddy');
 });
 
 module.exports = router;
